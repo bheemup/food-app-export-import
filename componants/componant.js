e52a7321f9083fad3124d32d1c303c2b3c04b227
bheemup <devendrakumar501425@gmail.com>
2022-05-01 13:02:45 +0530

    this is food app import export

---
+++ componants/componant.js
@@ -0,0 +1,14 @@
+function navbar(){
+    return `         <div>
+       <a href="index.html">Home</a>
+    </div>
+    <div>
+        <a href="today.html">Today Specail</a>
+        <a href="someother.html">Some other receipes</a>
+         <a href="signup.html">Sign Up </a>
+         <span> || <span/>
+         <a href="signin.html">Sign In </a>
+         
+    </div>`
+}
+ export default navbar;
